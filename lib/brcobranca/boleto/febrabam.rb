9ce53d2de19087e85294c754e26e161fea3b7b32
JeffVieira <jeff_malganis@hotmail.com>
2013-05-15 13:39:25 -0300

    comit bolet teste

--- lib/brcobranca/boleto/febrabam.rb
+++ lib/brcobranca/boleto/febrabam.rb
@@ -3,30 +3,12 @@ module Brcobranca
   module Boleto
     class Febrabam < Base # Banco do Brasil
 
-      validates_length_of :agencia, :maximum => 4, :message => "deve ser menor ou igual a 4 dígitos."
-      validates_length_of :conta_corrente, :maximum => 8, :message => "deve ser menor ou igual a 8 dígitos."
-      validates_length_of :carteira, :maximum => 2, :message => "deve ser menor ou igual a 2 dígitos."
-      validates_length_of :convenio, :in => 4..8, :message => "não existente para este banco."
-
-      validates_each :numero_documento do |record, attr, value|
-        valor_tamanho = value.to_s.size
-        registro_tamanho = record.convenio.to_s.size
-        quantidade = case
-        when (valor_tamanho > 9) && (registro_tamanho == 8)
-          '9'
-        when (valor_tamanho > 10) && (registro_tamanho == 7)
-          '10'
-        when (valor_tamanho > 7) && (registro_tamanho == 4)
-          '7'
-        when (valor_tamanho > 5) && (registro_tamanho == 6) && (!record.codigo_servico)
-          '5'
-        when (valor_tamanho > 17) && (registro_tamanho == 6) && (record.codigo_servico)
-          '17'
-        else
-          nil
-        end
-        record.errors.add attr, "deve ser menor ou igual a #{quantidade} dígitos." if quantidade
-      end
+      #validates_length_of :agencia, :maximum => 4, :message => "deve ser menor ou igual a 4 dígitos."
+      #validates_length_of :conta_corrente, :maximum => 8, :message => "deve ser menor ou igual a 8 dígitos."
+      #validates_length_of :carteira, :maximum => 2, :message => "deve ser menor ou igual a 2 dígitos."
+      validates_length_of :convenio, :in => 4, :message => "deve ser igual a 4 digitos."
+    
+      validates_length_of :numero_documento, :in => 1..17, :message => "deve conter ao menos um digito."
 
       # Nova instancia do BancoBrasil
       # @param (see Brcobranca::Boleto::Base#initialize)
@@ -39,7 +21,7 @@ module Brcobranca
       #
       # @return [String] 3 caracteres numéricos.
       def banco
-        "001"
+        ""
       end
 
       # Carteira
@@ -134,26 +116,50 @@ module Brcobranca
       end
 
       # Segunda parte do código de barras.
-      # A montagem é feita baseada na quantidade de dígitos do convênio.
-      # @return [String] 25 caracteres numéricos.
+      # @return [String] 40 caracteres numéricos.
       def codigo_barras_segunda_parte
-        case self.convenio.to_s.size
-        when 8 # Nosso Número de 17 dígitos com Convenio de 8 dígitos e numero_documento de 9 dígitos
-          "000000#{self.convenio}#{self.numero_documento}#{self.carteira}"
-        when 7 # Nosso Número de 17 dígitos com Convenio de 7 dígitos e numero_documento de 10 dígitos
-          "000000#{self.convenio}#{self.numero_documento}#{self.carteira}"
-        when 6 # Convenio de 6 dígitos
-          if self.codigo_servico == false
-            # Nosso Número de 11 dígitos com Convenio de 6 dígitos e numero_documento de 5 dígitos
-            "#{self.convenio}#{self.numero_documento}#{self.agencia}#{self.conta_corrente}#{self.carteira}"
-          else
-            # Nosso Número de 17 dígitos com Convenio de 6 dígitos e sem numero_documento, carteira 16 e 18
-            raise "Só é permitido emitir boletos com nosso número de 17 dígitos com carteiras 16 ou 18. Sua carteira atual é #{self.carteira}" unless (["16","18"].include?(self.carteira))
-            "#{self.convenio}#{self.numero_documento}21"
-          end
-        when 4 # Nosso Número de 7 dígitos com Convenio de 4 dígitos e sem numero_documento
-          "#{self.convenio}#{self.numero_documento}#{self.agencia}#{self.conta_corrente}#{self.carteira}"
+        "#{valor_documento_formatado}#{self.convenio}#{(DataTime.now + self.data_vencimento.days).to_date.strftime("%Y%m%d")}#{self.numero_documento}"
+      end
+      
+      # Monta a primeira parte do código de barras.
+      # Codigo padrão sendo
+      # Posição |Tamanho |Conteúdo<br/>
+      # 01 a 01 | 1  | Identificação do Produto Constante “8” para identificar arrecadação<br/>
+      # 02 a 02 | 1  | Identificação do Segmento "1" Prefeituras;<br/>
+      # 03 a 03 | 1  | Identificador de Valor Efetivo ou Referência "6" utiliza modulo de conversão 10<br/>
+      # @return [String] 3 caracteres numéricos.
+      def codigo_barras_primeira_parte
+        "816"
+      end
+      
+      def digito_verificador
+        raise Brcobranca::BoletoInvalido.new(self) unless self.valid?
+        codigo = codigo_barras_primeira_parte #18 digitos
+        codigo << codigo_barras_segunda_parte #25 digitos
+        if codigo =~ /^(\d{3})(\d{40})$/
+          codigo_dv = codigo.modulo10
+        else
+          raise Brcobranca::BoletoInvalido.new(self)
         end
+      end
+
+      # Codigo de barras do boleto
+      #
+      # O codigo de barra contém 44 posições dispostas:
+      def codigo_barras_sem_digitos_verrificadores
+        "#{codigo_barras_primeira_parte}#{digito_verificador}#{codigo_barras_segunda_parte}"
+      end
+      
+      
+      #Codigo de barras completo
+      #
+      #adicona para cada grupo de 11 digitos do codigo de barras o seu digito verificador no modulo 10
+      def codigo_barras
+        parte_codigo_11_digito = codigo_barras_sem_digitos_verrificadores.scan(/.........../)
+        parte_codigo_11_digito.each do |add_dv|
+          add_dv.concat(add_dv.modulo10)
+        end
+        parte_codigo_11_digito
       end
 
     end
